733794dda17fbf08cce0f09f66e7b95fa673cd6a
Preet Batavia <50803985+Preetbatavia@users.noreply.github.com>
2023-08-25 06:56:30 -0400

    Create gen_instruction.sv

---
+++ Constraints/gen_instruction.sv
@@ -0,0 +1,31 @@
+// Write a constraint that generates Add, mul, sub, nop instructions. Such that no Add instruction is repeated in 3 clock cycles and sub is not repeated in the last 3 valid instructions. 
+//Nop is not a valid instruction
+class val_instr; 
+  typedef enum {ADD, MUL , SUB , NOP} instr; 
+  randc instr ins; 
+  instr queue_add[$];
+  instr queue_sub[$];
+  
+  constraint add_c {(ins == ADD)->!(ins inside {queue_add});}
+  constraint sub_c {(ins == SUB)->!(ins inside {queue_sub});}
+  function void post_randomize();
+    queue_add.push_back(ins);
+    if(queue_add.size() == 3)
+      queue_add.pop_front();
+    if(ins == ADD || ins == SUB || ins == MUL)
+      queue_sub.push_back(ins); 
+    if(queue_sub.size() == 3)
+      queue_sub.pop_front();
+  endfunction 
+  
+endclass 
+
+module abc; 
+  initial begin 
+    val_instr val = new(); 
+    repeat(20)begin 
+      val.randomize(); 
+      $display("Instruction : %s", val.ins);
+    end 
+  end 
+endmodule 
